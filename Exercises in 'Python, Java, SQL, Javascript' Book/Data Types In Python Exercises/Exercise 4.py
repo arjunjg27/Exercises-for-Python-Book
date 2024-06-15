937926c81ba72745758343e385775fc23f9f2066
Arjun <159671145+arjunjg27@users.noreply.github.com>
2024-06-14 22:00:20 -0400

    Delete Exercises in 'Python, Java, SQL, Javascript' Book/Data Types In Python Exercises/Exercise 4.py

--- Exercises in 'Python, Java, SQL, Javascript' Book/Data Types In Python Exercises/Exercise 4.py
+++
@@ -1,6 +0,0 @@
-f = ['apple', 'banana', 'orange', 'peach', 'kiwi']
-user = input("What fruit do you like:")
-if user.lower() in f:
-    print("The fruit is in the list")
-if user.lower() not in f:
-    print("Your fruit is not in the list")
